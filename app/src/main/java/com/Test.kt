e28eb36a8563dcec37f5f634c616d9d416f54491
kkaa90 <kkaa90@naver.com>
2022-07-28 22:57:13 +0900

    테스트

--- app/src/main/java/com/Test.kt
+++ app/src/main/java/com/Test.kt
@@ -1,14 +1,17 @@
 package com
 
+import android.app.Activity
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.tooling.preview.Preview
 import com.ui.theme.WDCAPPTheme
 
@@ -31,7 +34,8 @@ class Test : ComponentActivity() {
 
 @Composable
 fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+    val context = LocalContext.current
+    Text(text = "Hello $name!", modifier = Modifier.clickable { (context as Activity).finish() })
 }
 
 @Preview(showBackground = true)
